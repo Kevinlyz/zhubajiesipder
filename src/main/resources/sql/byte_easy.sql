1bfc437a81665e70412fa530b9b882fdb713e6ba
liuxueqiverysmart <liuxueqismartbest@example.com>
2019-05-23 14:15:26 +0800

    add sql

--- src/main/resources/sql/byte_easy.sql
+++ src/main/resources/sql/byte_easy.sql
@@ -1,195 +1,239 @@
 /*
-Navicat MySQL Data Transfer
+ Navicat Premium Data Transfer
 
-Source Server         : localhost_3306
-Source Server Version : 50721
-Source Host           : localhost:3306
-Source Database       : byte_easy
+ Source Server         : 刘雪奇的库库
+ Source Server Type    : MySQL
+ Source Server Version : 50643
+ Source Host           : localhost:3306
+ Source Schema         : byte_easy
 
-Target Server Type    : MYSQL
-Target Server Version : 50721
-File Encoding         : 65001
+ Target Server Type    : MySQL
+ Target Server Version : 50643
+ File Encoding         : 65001
 
-Date: 2019-04-11 22:12:12
+ Date: 23/05/2019 11:21:55
 */
 
-SET FOREIGN_KEY_CHECKS=0;
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
 -- Table structure for persistent_logins
 -- ----------------------------
 DROP TABLE IF EXISTS `persistent_logins`;
-CREATE TABLE `persistent_logins` (
-  `series` varchar(255) NOT NULL,
-  `last_used` datetime DEFAULT NULL,
-  `token` varchar(255) NOT NULL,
-  `username` varchar(255) NOT NULL,
-  PRIMARY KEY (`series`),
-  UNIQUE KEY `UK_bqa9l0go97v49wwyx4c0u3kpd` (`token`),
-  UNIQUE KEY `UK_f8t9fsfwc17s6qcbx0ath6l3h` (`username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `persistent_logins`  (
+  `series` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `last_used` datetime(0) NULL DEFAULT NULL,
+  `token` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  PRIMARY KEY (`series`) USING BTREE,
+  UNIQUE INDEX `UK_bqa9l0go97v49wwyx4c0u3kpd`(`token`) USING BTREE,
+  UNIQUE INDEX `UK_f8t9fsfwc17s6qcbx0ath6l3h`(`username`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
--- Records of persistent_logins
+-- Table structure for sort
 -- ----------------------------
+DROP TABLE IF EXISTS `sort`;
+CREATE TABLE `sort`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `fenlei` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '分类',
+  `addr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '所属地区',
+  `creatdate` datetime(0) NULL DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 11 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Records of sort
+-- ----------------------------
+INSERT INTO `sort` VALUES (9, '软件开发', '北京', '2019-05-23 02:15:56');
 
 -- ----------------------------
 -- Table structure for sys_organization
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_organization`;
-CREATE TABLE `sys_organization` (
+CREATE TABLE `sys_organization`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `available` bit(1) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `parent_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FKmeds2u6ae5usj0ko0bqj3k0eo` (`parent_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of sys_organization
--- ----------------------------
+  `available` bit(1) NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `parent_id` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `FKmeds2u6ae5usj0ko0bqj3k0eo`(`parent_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Table structure for sys_resource
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_resource`;
-CREATE TABLE `sys_resource` (
+CREATE TABLE `sys_resource`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `available` bit(1) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
+  `available` bit(1) NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `order_num` int(11) NOT NULL,
-  `permission` varchar(255) DEFAULT NULL,
-  `type` int(11) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `parent_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK3fekum3ead5klp7y4lckn5ohi` (`parent_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=153 DEFAULT CHARSET=utf8;
+  `permission` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `type` int(11) NULL DEFAULT NULL,
+  `url` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `parent_id` bigint(20) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `FK3fekum3ead5klp7y4lckn5ohi`(`parent_id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 155 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_resource
 -- ----------------------------
-INSERT INTO `sys_resource` VALUES ('1', '\0', '顶级栏目', '100', null, '0', null, '0');
-INSERT INTO `sys_resource` VALUES ('2', '\0', '权限配置', '8', '', '0', '', '1');
-INSERT INTO `sys_resource` VALUES ('3', '\0', '角色管理', '102', '/role', '0', '/role', '2');
-INSERT INTO `sys_resource` VALUES ('4', '\0', '权限管理', '103', '/resource', '0', '/resource', '2');
-INSERT INTO `sys_resource` VALUES ('5', '\0', '用户管理', '101', '/user', '0', '/user', '2');
-INSERT INTO `sys_resource` VALUES ('6', '\0', '编辑', '100', '/role/editor-role', '1', '/role/editor-role', '3');
-INSERT INTO `sys_resource` VALUES ('7', '\0', '添加权限节点', '100', '/resource/add-permission', '1', '/resource/add-permission', '4');
-INSERT INTO `sys_resource` VALUES ('8', '\0', '添加管理员', '100', '/user/add-user', '1', '/user/add-user', '5');
-INSERT INTO `sys_resource` VALUES ('9', '\0', '添加角色', '100', '/role/add-role', '1', '/role/add-role', '3');
-INSERT INTO `sys_resource` VALUES ('10', '\0', '删除角色', '100', '/role/delete', '1', '/role/delete', '3');
-INSERT INTO `sys_resource` VALUES ('11', '\0', '删除用户', '100', '/user/delete-user', '1', '/user/delete-user', '5');
-INSERT INTO `sys_resource` VALUES ('12', '\0', '删除权限', '100', '/resource/delete', '1', '/resource/delete', '4');
-INSERT INTO `sys_resource` VALUES ('13', '\0', '启用', '100', '/user/available-user', '1', '/user/available-user', '3');
-INSERT INTO `sys_resource` VALUES ('14', '\0', '修改管理员密码', '100', '/user/modify-password', '1', '/user/modify-password', '5');
-INSERT INTO `sys_resource` VALUES ('16', null, '权限编辑', '100', '/resource/editor-permission', '1', '/resource/editor-permission', '4');
-INSERT INTO `sys_resource` VALUES ('150', '\0', '编辑管理员信息', '100', '/user/edit-user', '1', '/user/edit-user', '5');
-INSERT INTO `sys_resource` VALUES ('151', null, '人信息管理', '1', '', '0', '', '1');
-INSERT INTO `sys_resource` VALUES ('152', null, '人信息列表', '1', '/admin/person', '0', '/admin/person', '151');
+INSERT INTO `sys_resource` VALUES (1, b'0', '顶级栏目', 100, NULL, 0, NULL, 0);
+INSERT INTO `sys_resource` VALUES (2, b'0', '权限配置', 8, '', 0, '', 1);
+INSERT INTO `sys_resource` VALUES (3, b'0', '角色管理', 102, '/role', 0, '/role', 2);
+INSERT INTO `sys_resource` VALUES (4, b'0', '权限管理', 103, '/resource', 0, '/resource', 2);
+INSERT INTO `sys_resource` VALUES (5, b'0', '用户管理', 101, '/user', 0, '/user', 2);
+INSERT INTO `sys_resource` VALUES (6, b'0', '编辑', 100, '/role/editor-role', 1, '/role/editor-role', 3);
+INSERT INTO `sys_resource` VALUES (7, b'0', '添加权限节点', 100, '/resource/add-permission', 1, '/resource/add-permission', 4);
+INSERT INTO `sys_resource` VALUES (8, b'0', '添加管理员', 100, '/user/add-user', 1, '/user/add-user', 5);
+INSERT INTO `sys_resource` VALUES (9, b'0', '添加角色', 100, '/role/add-role', 1, '/role/add-role', 3);
+INSERT INTO `sys_resource` VALUES (10, b'0', '删除角色', 100, '/role/delete', 1, '/role/delete', 3);
+INSERT INTO `sys_resource` VALUES (11, b'0', '删除用户', 100, '/user/delete-user', 1, '/user/delete-user', 5);
+INSERT INTO `sys_resource` VALUES (12, b'0', '删除权限', 100, '/resource/delete', 1, '/resource/delete', 4);
+INSERT INTO `sys_resource` VALUES (13, b'0', '启用', 100, '/user/available-user', 1, '/user/available-user', 3);
+INSERT INTO `sys_resource` VALUES (14, b'0', '修改管理员密码', 100, '/user/modify-password', 1, '/user/modify-password', 5);
+INSERT INTO `sys_resource` VALUES (16, NULL, '权限编辑', 100, '/resource/editor-permission', 1, '/resource/editor-permission', 4);
+INSERT INTO `sys_resource` VALUES (150, b'0', '编辑管理员信息', 100, '/user/edit-user', 1, '/user/edit-user', 5);
+INSERT INTO `sys_resource` VALUES (151, NULL, '人信息管理', 2, '', 0, '', 1);
+INSERT INTO `sys_resource` VALUES (152, NULL, '人信息列表', 1, '/admin/person', 0, '/admin/person', 151);
+INSERT INTO `sys_resource` VALUES (153, NULL, '爬取信息管理', 1, '', 0, '', 1);
+INSERT INTO `sys_resource` VALUES (154, NULL, '爬取猪八戒信息', 1, '/admin/sort', 0, '/admin/sort', 153);
 
 -- ----------------------------
 -- Table structure for sys_role
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role`;
-CREATE TABLE `sys_role` (
+CREATE TABLE `sys_role`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `available` bit(1) DEFAULT NULL,
-  `description` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `available` bit(1) NULL DEFAULT NULL,
+  `description` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_role
 -- ----------------------------
-INSERT INTO `sys_role` VALUES ('1', null, '管理员', '管理员');
+INSERT INTO `sys_role` VALUES (1, NULL, '管理员', '管理员');
 
 -- ----------------------------
 -- Table structure for sys_role_resources
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_role_resources`;
-CREATE TABLE `sys_role_resources` (
+CREATE TABLE `sys_role_resources`  (
   `sys_role_id` bigint(20) NOT NULL,
   `resources_id` bigint(20) NOT NULL,
-  KEY `FKog6jj4v6yh9e1ilxk2mwuk75a` (`resources_id`),
-  KEY `FKsqkqfd2hpr5cc2kbrtgoced2w` (`sys_role_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  INDEX `FKog6jj4v6yh9e1ilxk2mwuk75a`(`resources_id`) USING BTREE,
+  INDEX `FKsqkqfd2hpr5cc2kbrtgoced2w`(`sys_role_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_role_resources
 -- ----------------------------
-INSERT INTO `sys_role_resources` VALUES ('1', '2');
-INSERT INTO `sys_role_resources` VALUES ('1', '3');
-INSERT INTO `sys_role_resources` VALUES ('1', '6');
-INSERT INTO `sys_role_resources` VALUES ('1', '9');
-INSERT INTO `sys_role_resources` VALUES ('1', '10');
-INSERT INTO `sys_role_resources` VALUES ('1', '13');
-INSERT INTO `sys_role_resources` VALUES ('1', '4');
-INSERT INTO `sys_role_resources` VALUES ('1', '7');
-INSERT INTO `sys_role_resources` VALUES ('1', '12');
-INSERT INTO `sys_role_resources` VALUES ('1', '16');
-INSERT INTO `sys_role_resources` VALUES ('1', '5');
-INSERT INTO `sys_role_resources` VALUES ('1', '8');
-INSERT INTO `sys_role_resources` VALUES ('1', '11');
-INSERT INTO `sys_role_resources` VALUES ('1', '14');
-INSERT INTO `sys_role_resources` VALUES ('1', '150');
-INSERT INTO `sys_role_resources` VALUES ('1', '151');
-INSERT INTO `sys_role_resources` VALUES ('1', '152');
+INSERT INTO `sys_role_resources` VALUES (1, 2);
+INSERT INTO `sys_role_resources` VALUES (1, 3);
+INSERT INTO `sys_role_resources` VALUES (1, 6);
+INSERT INTO `sys_role_resources` VALUES (1, 9);
+INSERT INTO `sys_role_resources` VALUES (1, 10);
+INSERT INTO `sys_role_resources` VALUES (1, 13);
+INSERT INTO `sys_role_resources` VALUES (1, 4);
+INSERT INTO `sys_role_resources` VALUES (1, 7);
+INSERT INTO `sys_role_resources` VALUES (1, 12);
+INSERT INTO `sys_role_resources` VALUES (1, 16);
+INSERT INTO `sys_role_resources` VALUES (1, 5);
+INSERT INTO `sys_role_resources` VALUES (1, 8);
+INSERT INTO `sys_role_resources` VALUES (1, 11);
+INSERT INTO `sys_role_resources` VALUES (1, 14);
+INSERT INTO `sys_role_resources` VALUES (1, 150);
+INSERT INTO `sys_role_resources` VALUES (1, 153);
+INSERT INTO `sys_role_resources` VALUES (1, 154);
 
 -- ----------------------------
 -- Table structure for sys_user
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user`;
-CREATE TABLE `sys_user` (
+CREATE TABLE `sys_user`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `createtime` datetime DEFAULT NULL,
-  `password` varchar(255) DEFAULT NULL,
-  `updatetime` datetime DEFAULT NULL,
-  `username` varchar(255) DEFAULT NULL,
-  `available` bit(1) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `tel` varchar(255) DEFAULT NULL,
-  `sex_type` int(11) DEFAULT NULL COMMENT '性别(0.男,1.女)',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  `createtime` datetime(0) NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `updatetime` datetime(0) NULL DEFAULT NULL,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `available` bit(1) NULL DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `tel` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `sex_type` int(11) NULL DEFAULT NULL COMMENT '性别(0.男,1.女)',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_user
 -- ----------------------------
-INSERT INTO `sys_user` VALUES ('1', '2017-07-11 17:42:18', '$2a$10$SIU57gfkh/TsIVYALXBNAeDnQzkm652FT9cg4h8wtEfC306uliyYa', '2019-01-11 07:34:38', 'admin', '', '1191134106@qq.com', '15030103078', '1');
+INSERT INTO `sys_user` VALUES (1, '2017-07-11 17:42:18', '$2a$10$SIU57gfkh/TsIVYALXBNAeDnQzkm652FT9cg4h8wtEfC306uliyYa', '2019-01-11 07:34:38', 'admin', b'1', '1191134106@qq.com', '15030103078', 1);
 
 -- ----------------------------
 -- Table structure for sys_user_roles
 -- ----------------------------
 DROP TABLE IF EXISTS `sys_user_roles`;
-CREATE TABLE `sys_user_roles` (
+CREATE TABLE `sys_user_roles`  (
   `sys_user_id` bigint(20) NOT NULL,
   `roles_id` bigint(20) NOT NULL,
-  KEY `FKdpvc6d7xqpqr43dfuk1s27cqh` (`roles_id`),
-  KEY `FKd0ut7sloes191bygyf7a3pk52` (`sys_user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  INDEX `FKdpvc6d7xqpqr43dfuk1s27cqh`(`roles_id`) USING BTREE,
+  INDEX `FKd0ut7sloes191bygyf7a3pk52`(`sys_user_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of sys_user_roles
 -- ----------------------------
-INSERT INTO `sys_user_roles` VALUES ('1', '1');
+INSERT INTO `sys_user_roles` VALUES (1, 1);
 
 -- ----------------------------
 -- Table structure for t_person
 -- ----------------------------
 DROP TABLE IF EXISTS `t_person`;
-CREATE TABLE `t_person` (
+CREATE TABLE `t_person`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL COMMENT '姓名',
-  `code` varchar(100) DEFAULT NULL COMMENT '编号',
-  `content` text COMMENT '内容',
-  `file_path` varchar(100) DEFAULT NULL COMMENT '图片',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  `name` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `code` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '编号',
+  `content` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL COMMENT '内容',
+  `file_path` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '图片',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of t_person
 -- ----------------------------
-INSERT INTO `t_person` VALUES ('5', '123', '123123', '<p><img src=\"http://localhost:8080/fileSuffix/20190403/20190403150850813_CBJCS6.png\" title=\"upfile\" alt=\"系统快捷键.png\"/></p>', '12312');
+INSERT INTO `t_person` VALUES (2, '2342342', '23423', NULL, NULL);
+INSERT INTO `t_person` VALUES (3, '123', '123', '<p><img src=\"http://localhost:8080/fileSuffix/20190401/20190401093914676_KY6JV8.png\" title=\"upfile\" alt=\"系统快捷键.png\"/></p>', NULL);
+INSERT INTO `t_person` VALUES (4, '123', '123', '<p>123123<br/></p>', '/fileSuffix/20190401/20190401094629111_8CNQSC.jpg');
+
+-- ----------------------------
+-- Table structure for title
+-- ----------------------------
+DROP TABLE IF EXISTS `title`;
+CREATE TABLE `title`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '标题',
+  `href` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '链接',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 332 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+-- ----------------------------
+-- Table structure for zbj
+-- ----------------------------
+DROP TABLE IF EXISTS `zbj`;
+CREATE TABLE `zbj`  (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `fenlei` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '分类',
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '公司名称',
+  `addr` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '地区',
+  `link` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '链接',
+  `type` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '类型',
+  `credit` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '信誉度',
+  `score` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '综合评分',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5506 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Compact;
+
+SET FOREIGN_KEY_CHECKS = 1;
